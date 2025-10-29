2bf02c3496fbf4fd964d270b3a69fb8e137a3db8
Agus Borgo <agusborgo5@gmail.com>
2025-10-28 23:57:45 -0300

    Creamos tabla con datos de http

--- src/components/Http.vue
+++ src/components/Http.vue
@@ -5,13 +5,42 @@
     </div>
 
     <div class="card-body">
-       
+       <button class="btn btn-primary my-3" @click="obtener">
+        Obtener Posts
+       </button>
+       <!-- <pre><p>{{ posts }}</p></pre> -->
+
+       <div v-if="posts.length">
+        <div class="table-responsive">
+       <table class="table table-dark">
+        <thead>
+          <tr>
+            <th>userId</th>
+            <th>id</th>
+            <th>title</th>
+            <th>body</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(post, index) in posts" :key="index">
+            <td>{{ post.userId }}</td>
+            <td>{{ post.id }}</td>
+            <td>{{ post.title }}</td>
+            <td>{{ post.body }}</td>
+          </tr>
+        </tbody>
+       </table>
+       </div>
+       </div>
+       <h4 v-else> Pedir los post a traves del boton</h4>
     </div>
   </section>
 </template>
 
 <script>
 import '../Servicios/posts.js'
+import { obtenerPosts } from '../Servicios/posts.js';
 export default {
   name: 'ComponenteHttp',
 
@@ -21,7 +50,8 @@ export default {
 
   data() {
     return {
-      
+      posts: [
+      ],
     };
   },
 
@@ -34,9 +64,17 @@ export default {
   },
 
   methods: {
-    
+    async obtener(){
+      const post = await obtenerPosts()
+      console.log(post);
+      this.posts = post;
+    }
   },
 
+  // ------------ LIFECYCLE HOOKS ------------
+  // Estos son los hooks del ciclo de vida del componente
+ // Puedes usar estos hooks para ejecutar código en diferentes etapas del ciclo de vida del componente
+  // -----------------------------------------
   created() {
     // Código que se ejecuta cuando el componente se crea
   },
